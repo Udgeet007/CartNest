6635e8586d2eab084bda1e3b72ce9ff556a9269c
Pratham Sahu <prathamsahu051@gmail.com>
2023-08-17 15:41:56 +0530

    Environment setup

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p>Hello Champ! Are you ready?</p>
+      <div className="p-4">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
